eca26fa601c8f683acb01417c1db958990e9a76b
Denis <denchik7366@gmail.com>
2022-09-13 23:56:43 +0400

    deploy

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Burger
+
+const menuBody = document.querySelector('.menu__body');
+const iconMenu = document.querySelector('.icon-menu');
+
+iconMenu.addEventListener('click', () => {
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+})
